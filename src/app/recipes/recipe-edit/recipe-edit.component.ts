8f12916ff2d0217e37ebe9149df4e3f7a10cb949
Caleb Elliott <calebelliott070@gmail.com>
2017-06-21 11:09:55 -0600

    Linked the HTML to the form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -27,6 +27,10 @@ export class RecipeEditComponent implements OnInit {
       );
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
@@ -36,7 +40,7 @@ export class RecipeEditComponent implements OnInit {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
-      recipeDescription = recipeDescription;
+      recipeDescription = recipe.description;
     }
 
     this.recipeForm = new FormGroup({
