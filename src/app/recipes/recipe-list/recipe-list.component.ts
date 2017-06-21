f0e202c735b7863312748e67f69cd730ba6e5237
Caleb Elliott <calebelliott070@gmail.com>
2017-06-21 15:20:00 -0600

    Fixed bug, delete ingredient now works

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
+  subscription: Subscription;
   //   new Recipe('Test Recipe', 'A test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
   //   new Recipe('Test Recipe 2', 'A test recipe', 'http://soupofsuccess.com/wp-content/uploads/2011/07/French-Cranberry-Chicken-Recipe.jpg')
   // ];
@@ -22,7 +24,7 @@ export class RecipeListComponent implements OnInit {
               private  route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.recipeService.recipesChanged
+    this.subscription = this.recipeService.recipesChanged
       .subscribe(
         (recipes: Recipe[]) => {
           this.recipes = recipes;
@@ -31,8 +33,11 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onNewRecipe(){
+  onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route})
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
