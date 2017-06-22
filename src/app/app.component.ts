71763afbde096d8e72a0143a108b3c221967e6b0
Caleb Elliott <calebelliott070@gmail.com>
2017-06-22 10:45:57 -0600

    added primitive auth service and linked to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
+
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDhyWfgc_9hOaErrBTD-a4_SzkJf0wZArw',
+      authDomain: 'recipe-book-ee4b1.firebaseapp.com'
+    })
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
+
 }
